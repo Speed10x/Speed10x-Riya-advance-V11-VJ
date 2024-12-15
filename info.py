bcaea66444b15c8a09077f511d05399fbe3f6e41
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2024-12-16 03:58:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,9 +24,9 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://graph.org/file/f8b67added3262575819a-089af052a391b2d5bc.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/testingdb2/10")
+MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/f8b67added3262575819a-089af052a391b2d5bc.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
